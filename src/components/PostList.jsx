22b5aa309d03fdc64e4428d3e2e231eddf2f8667
alekseevnagirl <kashitsyna.2001@mail.ru>
2026-05-21 13:44:13 +0400

    add app

---
+++ src/components/PostList.jsx
@@ -0,0 +1,48 @@
+import React, {useRef} from "react";
+import PostItem from "./PostItem";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
+
+const PostList = ({posts, title, remove}) => {
+  
+  const itemRefs = useRef({});
+
+  if (!posts.length) {
+    return (
+      <h1 style={{textAlign: 'center'}}>
+        Посты не найдены!
+      </h1>
+    )
+  }
+
+  return (
+    <div>
+      <h1 style={{textAlign: 'center'}}>
+        {title}
+      </h1>
+      <TransitionGroup>
+        {posts.map((post, index) => {
+          if (!itemRefs.current[post.id]) {
+            itemRefs.current[post.id] = React.createRef();
+          }
+          const itemRef = itemRefs.current[post.id];
+
+          return (
+            <CSSTransition
+              key={post.id}
+              nodeRef={itemRef}
+              timeout={500}
+              classNames="post"
+            >
+              <div ref={itemRef} classNames="post">
+                <PostItem number={index + 1} post={post} remove={remove} />
+              </div>
+            </CSSTransition>
+          );
+          }        
+        )}
+      </TransitionGroup>
+    </div>
+  );
+};
+
+export default PostList;
