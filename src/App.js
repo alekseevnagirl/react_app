266a087aedb0e79951c07668faeec29afea02d2b
alekseevnagirl <kashitsyna.2001@mail.ru>
2026-05-26 15:06:22 +0400

    add fetch posts

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import PostFilter from "./components/PostFilter";
 import MyModal from "./components/UI/MyModal/MyModal";
 import MyButton from "./components/UI/button/MyButton";
 import { usePosts } from "./hooks/usePosts";
+import axios from "axios";
 
 function App() {
   const [posts, setPosts] = useState([])
@@ -18,12 +19,18 @@ function App() {
     setModal(false)
   }
 
+  async function fetchPosts() {
+    const response = await axios.get('https://jsonplaceholder.typicode.com/posts')
+    setPosts(response.data)
+  }
+
   const removePost = (post) => {
     setPosts(posts.filter(p => p.id !== post.id))
   }
 
   return (
     <div className="App">
+      <MyButton onClick={fetchPosts}>Загрузить посты</MyButton>
       <MyButton style={{marginTop: 30}} onClick={() => setModal(true)}>
         Создать пост
       </MyButton>
@@ -35,7 +42,11 @@ function App() {
         filter={filter} 
         setFilter={setFilter} 
       />
-      <PostList posts={sortedAndSearchedPosts} title={"Посты про JS"} remove={removePost} />
+      <PostList 
+        posts={sortedAndSearchedPosts} 
+        title={"Посты про JS"} 
+        remove={removePost} 
+      />
     </div>
   );
 }
