5391f21fa738189170a56f9ea6ac58ecf0bb06fe
alekseevnagirl <kashitsyna.2001@mail.ru>
2026-06-05 15:10:31 +0400

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React from "react";
 import './styles/App.css';
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Link, Navigate, Route, Routes } from "react-router-dom";
 import About from "./pages/About";
 import Posts from "./pages/Posts";
+import Navbar from "./components/UI/Navbar/Navbar";
 
 function App() {
   return (
     <BrowserRouter>
+      <Navbar />
       <Routes>
         <Route path="/about" element={<About />}>
         </Route>
 
         <Route path="/posts" element={<Posts />}>
         </Route>
+
+        <Route path="*" element={<Navigate to="/posts" replace />} />
       </Routes>
     </BrowserRouter>
   );
