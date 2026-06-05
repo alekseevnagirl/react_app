0b605bb9a68b47f76e4b2add865bc3b0f7a61989
alekseevnagirl <kashitsyna.2001@mail.ru>
2026-06-05 15:23:17 +0400

    add app router

--- src/App.js
+++ src/App.js
@@ -1,27 +1,14 @@
 import React from "react";
 import './styles/App.css';
-import { BrowserRouter, Link, Navigate, Route, Routes } from "react-router-dom";
-import About from "./pages/About";
-import Error from "./pages/Error";
-import Posts from "./pages/Posts";
+import { BrowserRouter } from "react-router-dom";
 import Navbar from "./components/UI/Navbar/Navbar";
+import AppRouter from "./components/AppRouter";
 
 function App() {
   return (
     <BrowserRouter>
       <Navbar />
-      <Routes>
-        <Route path="/about" element={<About />}>
-        </Route>
-
-        <Route path="/posts" element={<Posts />}>
-        </Route>
-
-        <Route path="/error" element={<Error />}>
-        </Route>
-
-        <Route path="*" element={<Navigate to="/error" replace />} />
-      </Routes>
+      <AppRouter />
     </BrowserRouter>
   );
 }
